fa124ac6a14ceb525c99bf82eb9f1b93e78f64bf
Fernando Pombeiro <fernpombeiro@yahoo.com>
2022-05-04 15:44:02 +0100

    Updated to use s3 exclusively

--- cloudfront.tf
+++ cloudfront.tf
@@ -5,21 +5,20 @@ resource "aws_cloudfront_distribution" "cf" {
   // origin is where CloudFront gets its content from.
   comment = "Cloudfront Distribution for ${var.client} in the ${var.environment} environment"
   origin {
-    // We need to set up a "custom" origin because otherwise CloudFront won't
-    // redirect traffic from the root domain to the www domain, that is from
-    // runatlantis.io to www.runatlantis.io.
-    custom_origin_config {
-      // These are all the defaults.
-      http_port              = "80"
-      https_port             = "443"
-      origin_protocol_policy = "http-only"
-      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
-    }
+    # custom_origin_config {
+    #   // These are all the defaults.
+    #   http_port              = "80"
+    #   https_port             = "443"
+    #   origin_protocol_policy = "http-only"
+    #   origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    # }
 
-    // Here we're using our S3 bucket's URL!
     domain_name = var.s3bucket_endpoint
     // This can be any name to identify this origin.
     origin_id   = var.domain_name
+    s3_origin_config {
+      origin_access_identity = "origin-access-identity/cloudfront/ABCDEFG1234567"
+    }
   }
 
   enabled             = true
@@ -29,8 +28,8 @@ resource "aws_cloudfront_distribution" "cf" {
   default_cache_behavior {
     viewer_protocol_policy = "redirect-to-https"
     compress               = true
-    allowed_methods        = ["GET", "HEAD"]
-    cached_methods         = ["GET", "HEAD"]
+    allowed_methods        = ["GET", "HEAD", "OPTIONS"]
+    cached_methods         = ["GET", "HEAD", "OPTIONS"]
     // This needs to match the `origin_id` above.
     target_origin_id       = var.domain_name
     min_ttl                = 0
